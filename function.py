70f39fbc69c6b28fa64d3a68008c53d6b94eb75d
Sainivaishu <Vaishalisaini58197@gmail.com>
2024-05-25 14:26:09 +0000

    find me here

---
+++ function.py
@@ -0,0 +1,16 @@
+# def print_numbers(N):
+#     for i in range(1,N+1):
+#         print(i)
+# N=int(input("Enter a number: "))
+# print("Numbers 1 to N",N)
+# print_numbers(N)
+
+
+# def multiplication(number):
+#     print("multiplication table for ", (number))
+#     for i in range(1,11):
+#             print(number, "x", i,"=" ,number*i)
+# number=int(input("enter a number: "))
+# multiplication(number)
+
+
