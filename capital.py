f9153eaac72317c6f7ef172cc3f83cb6de056fab
Sainivaishu <Vaishalisaini58197@gmail.com>
2024-05-28 05:09:40 +0000

    hyy this is coding

---
+++ capital.py
@@ -0,0 +1,17 @@
+# a="be a master"
+# b=a.capitalize()
+# print((b)
+
+# a="welcome to my world"
+# b=a.title()
+# print(b)
+
+
+a="Hello world "
+b="hello 123 "
+c="BE A LIGHT"
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
+
+
